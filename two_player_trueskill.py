7c0aa03184563e11b8983993995d0c680b57db61
Jeremy Kun <j2kun@users.noreply.github.com>
2022-02-26 10:56:19 -0800

    two_player: add draw and simplify formulas

--- two_player_trueskill.py
+++ two_player_trueskill.py
@@ -45,19 +45,70 @@ def compute_draw_margin() -> float:
     return inv_cdf_output * sqrt(2 * SKILL_CLASS_WIDTH)
 
 
-def truncated_gaussian_v(perf_diff, draw_margin):
-    normalization = STANDARD_NORMAL.cdf(perf_diff - draw_margin)
+# TODO: add plots for these...
+def truncated_onesided_gaussian_v(t: float, lower: float) -> float:
+    '''Equation 4.7 from Herbrich '05, On Gaussian Expectation Propagation.
+
+    Representing the additive correction factor to the mean of a rectified
+    Gaussian that is only truncated from below.
+
+    This function grows roughly like lower - t for t < lower and quickly
+    approaches zero for t > lower.
+    '''
+    normalization = STANDARD_NORMAL.cdf(t - lower)
     if normalization < TOLERANCE:
-        return -perf_diff + draw_margin
-    return STANDARD_NORMAL.pdf(perf_diff - draw_margin) / normalization
+        return lower - t   # approaches infinity
+    return STANDARD_NORMAL.pdf(t - lower) / normalization
 
 
-def truncated_gaussian_w(perf_diff, draw_margin):
-    normalization = STANDARD_NORMAL.cdf(perf_diff - draw_margin)
+def truncated_onesided_gaussian_w(t: float, lower: float) -> float:
+    '''Equation 4.8 from Herbrich '05, On Gaussian Expectation Propagation.
+
+    Representing the additive correction factor to the variance of a rectified
+    Gaussian that is only truncated from below. This is a smooth approximation
+    to an indicator function for the condition t <= lower.
+    '''
+    v_value = truncated_onesided_gaussian_v(t, lower)
+    return v_value * (v_value + t - lower)
+
+
+def truncated_twosided_gaussian_v(t, lower, upper):
+    '''Equation 4.4 from Herbrich '05, On Gaussian Expectation Propagation.
+
+    Representing the additive correction factor to the mean of a rectified
+    Gaussian that is truncated on both sides.
+    '''
+    normalization = (STANDARD_NORMAL.cdf(upper - t) -
+                     STANDARD_NORMAL.cdf(lower - t))
     if normalization < TOLERANCE:
-        return int(perf_diff < 0)
-    v_value = truncated_gaussian_v(perf_diff, draw_margin)
-    return v_value * (v_value + perf_diff - draw_margin)
+        # the limit as upper -> lower
+        return lower - t
+
+    return (
+        STANDARD_NORMAL.pdf(lower - t) - STANDARD_NORMAL.pdf(upper - t)
+    ) / normalization
+
+
+def truncated_twosided_gaussian_w(perf_diff, draw_margin):
+    '''Equation 4.4 from Herbrich '05, On Gaussian Expectation Propagation.
+
+    Representing the additive correction factor to the variance of a rectified
+    Gaussian that is truncated on both sides.
+    '''
+    abs_diff = abs(perf_diff)
+    normalization = (
+        STANDARD_NORMAL.cdf(draw_margin - abs_diff) -
+        STANDARD_NORMAL.cdf(-draw_margin - abs_diff)
+    )
+    if normalization < TOLERANCE:
+        return 1
+
+    v_value = truncated_twosided_gaussian_v(
+        perf_diff, -draw_margin, draw_margin)
+    t1 = (draw_margin - abs_diff) * STANDARD_NORMAL.pdf(draw_margin - abs_diff)
+    t2 = (-draw_margin - abs_diff) * \
+        STANDARD_NORMAL.pdf(-draw_margin - abs_diff)
+    return v_value ** 2 + (t1 - t2) / normalization
 
 
 def update_one_player(p1_rating, p2_rating, outcome):
@@ -67,15 +118,21 @@ def update_one_player(p1_rating, p2_rating, outcome):
     losing_mean = p2_rating.mean if outcome >= 0 else p1_rating.mean
     perf_diff = winning_mean - losing_mean
 
-    # TODO: add draw case
-    v = truncated_gaussian_v(perf_diff / c, draw_margin / c)
-    w = truncated_gaussian_w(perf_diff / c, draw_margin / c)
+    if outcome == 0:
+        v = truncated_twosided_gaussian_v(
+            perf_diff / c, -draw_margin / c, draw_margin / c)
+        w = truncated_twosided_gaussian_w(perf_diff / c, draw_margin / c)
+        mean_adjustment_direction = 1
+    else:
+        v = truncated_onesided_gaussian_v(perf_diff / c, draw_margin / c)
+        w = truncated_onesided_gaussian_w(perf_diff / c, draw_margin / c)
+        mean_adjustment_direction = outcome
 
     mean_multiplier = (p1_rating.mean ** 2 + ADDITIVE_DYNAMICS_FACTOR) / c
     variance_plus_dynamics = p1_rating.stddev ** 2 + ADDITIVE_DYNAMICS_FACTOR
     stddev_multiplier = variance_plus_dynamics / (c ** 2)
 
-    new_mean = p1_rating.mean + outcome * mean_multiplier * v
+    new_mean = p1_rating.mean + mean_adjustment_direction * mean_multiplier * v
     new_stddev = sqrt(variance_plus_dynamics*(1 - w * stddev_multiplier))
 
     return Rating(mean=new_mean, stddev=new_stddev)
--- two_player_trueskill_test.py
+++ two_player_trueskill_test.py
@@ -47,3 +47,66 @@ def test_surprising_p1_loss():
     # now we're more uncertain about both ratings due to the upset
     assert new_p1.stddev > p1_rating.stddev
     assert new_p2.stddev > p2_rating.stddev
+
+
+def test_equal_player_draw():
+    p1_rating = Rating(mean=10, stddev=0.1)
+    p2_rating = Rating(mean=10, stddev=0.1)
+    outcome = 0
+    new_p1, new_p2 = update_ratings(p1_rating, p2_rating, outcome)
+
+    # draw is the exact expected outcome, implying no rating adjustment
+    assert new_p1.mean == p1_rating.mean
+    assert new_p2.mean == p2_rating.mean
+
+    # now we're more uncertain simply because time has passed
+    # and we haven't gained any more information from the draw
+    assert new_p1.stddev > p1_rating.stddev
+    assert new_p2.stddev > p2_rating.stddev
+
+
+def test_unequal_draw():
+    p1_rating = Rating(mean=2, stddev=0.1)
+    p2_rating = Rating(mean=10, stddev=0.1)
+    outcome = 0
+    new_p1, new_p2 = update_ratings(p1_rating, p2_rating, outcome)
+
+    # draw is surprising, given P1 is supposed to be worse than P2
+    assert new_p1.mean > p1_rating.mean
+    assert new_p2.mean < p2_rating.mean
+
+
+def test_high_variance_win():
+    p1_rating = Rating(mean=10, stddev=10)
+    p2_rating = Rating(mean=10, stddev=10)
+    outcome = 1
+    new_p1, new_p2 = update_ratings(p1_rating, p2_rating, outcome)
+
+    assert new_p1.mean > p1_rating.mean
+    assert new_p2.mean < p2_rating.mean
+
+    # variances decrease because we got more information.
+    assert new_p1.stddev < p1_rating.stddev
+    assert new_p2.stddev < p2_rating.stddev
+
+
+def test_huge_upset_via_win():
+    # this test exists to test the boundaries of the CDF/PDF functions
+    # and the limiting formula to handle them.
+    p1_rating = Rating(mean=10, stddev=1)
+    p2_rating = Rating(mean=100, stddev=1)
+    outcome = 1
+    new_p1, new_p2 = update_ratings(p1_rating, p2_rating, outcome)
+    assert new_p1.mean > p2_rating.mean
+    assert new_p2.mean < p1_rating.mean
+
+
+def test_huge_upset_via_draw():
+    # this test exists to test the boundaries of the CDF/PDF functions
+    # and the limiting formula to handle them.
+    p1_rating = Rating(mean=10, stddev=1)
+    p2_rating = Rating(mean=100, stddev=1)
+    outcome = 0
+    new_p1, new_p2 = update_ratings(p1_rating, p2_rating, outcome)
+    assert new_p1.mean > p1_rating.mean
+    assert new_p2.mean < p2_rating.mean
