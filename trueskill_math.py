a86997e5cb6f3b1dddf0de4a9dfb592b5988628b
Jeremy Kun <j2kun@users.noreply.github.com>
2022-02-26 11:24:38 -0800

    refactor constants and math formulas for reuse with team impl

---
+++ trueskill_math.py
@@ -0,0 +1,96 @@
+'''Math functions for hard-coded formulas used in inference steps.
+'''
+from math import sqrt
+from statistics import NormalDist
+
+from constants import DRAW_PROBABILITY
+from constants import SKILL_CLASS_WIDTH
+from constants import TOLERANCE
+
+STANDARD_NORMAL = NormalDist(0, 1)
+
+
+def compute_draw_margin() -> float:
+    '''
+      The margin to use to consider the game a draw, based on the (pre-set)
+      probability of a draw, which is typically set by measuring the draw rates
+      of a large number of games. Derived by inverting the formula
+
+        P(draw) = -1 + 2 * normal_cdf(
+            draw_margin / sqrt(player_count * skill_class_width)
+        )
+
+      or, as written exactly in the paper,
+
+        P(draw) = -1 + 2 * normal_cdf(
+            draw_margin / (sqrt(n1 + n2) * beta)
+        )
+    '''
+    inv_cdf_arg = 0.5 * (DRAW_PROBABILITY + 1)
+    inv_cdf_output = STANDARD_NORMAL.inv_cdf(inv_cdf_arg)
+    return inv_cdf_output * sqrt(2 * SKILL_CLASS_WIDTH)
+
+
+def truncated_onesided_gaussian_v(t: float, lower: float) -> float:
+    '''Equation 4.7 from Herbrich '05, On Gaussian Expectation Propagation.
+
+    Representing the additive correction factor to the mean of a rectified
+    Gaussian that is only truncated from below.
+
+    This function grows roughly like lower - t for t < lower and quickly
+    approaches zero for t > lower.
+    '''
+    normalization = STANDARD_NORMAL.cdf(t - lower)
+    if normalization < TOLERANCE:
+        return lower - t   # approaches infinity
+    return STANDARD_NORMAL.pdf(t - lower) / normalization
+
+
+def truncated_onesided_gaussian_w(t: float, lower: float) -> float:
+    '''Equation 4.8 from Herbrich '05, On Gaussian Expectation Propagation.
+
+    Representing the additive correction factor to the variance of a rectified
+    Gaussian that is only truncated from below. This is a smooth approximation
+    to an indicator function for the condition t <= lower.
+    '''
+    v_value = truncated_onesided_gaussian_v(t, lower)
+    return v_value * (v_value + t - lower)
+
+
+def truncated_twosided_gaussian_v(t, lower, upper):
+    '''Equation 4.4 from Herbrich '05, On Gaussian Expectation Propagation.
+
+    Representing the additive correction factor to the mean of a rectified
+    Gaussian that is truncated on both sides.
+    '''
+    normalization = (STANDARD_NORMAL.cdf(upper - t) -
+                     STANDARD_NORMAL.cdf(lower - t))
+    if normalization < TOLERANCE:
+        # the limit as upper -> lower
+        return lower - t
+
+    return (
+        STANDARD_NORMAL.pdf(lower - t) - STANDARD_NORMAL.pdf(upper - t)
+    ) / normalization
+
+
+def truncated_twosided_gaussian_w(perf_diff, draw_margin):
+    '''Equation 4.4 from Herbrich '05, On Gaussian Expectation Propagation.
+
+    Representing the additive correction factor to the variance of a rectified
+    Gaussian that is truncated on both sides.
+    '''
+    abs_diff = abs(perf_diff)
+    normalization = (
+        STANDARD_NORMAL.cdf(draw_margin - abs_diff) -
+        STANDARD_NORMAL.cdf(-draw_margin - abs_diff)
+    )
+    if normalization < TOLERANCE:
+        return 1
+
+    v_value = truncated_twosided_gaussian_v(
+        perf_diff, -draw_margin, draw_margin)
+    t1 = (draw_margin - abs_diff) * STANDARD_NORMAL.pdf(draw_margin - abs_diff)
+    t2 = (-draw_margin - abs_diff) * \
+        STANDARD_NORMAL.pdf(-draw_margin - abs_diff)
+    return v_value ** 2 + (t1 - t2) / normalization
--- two_player_trueskill.py
+++ two_player_trueskill.py
@@ -2,113 +2,23 @@
 
 from dataclasses import dataclass
 from math import sqrt
-from statistics import NormalDist
 from typing import Tuple
 
-
-DEFAULT_MEAN = 25
-DEFAULT_STD_DEV = DEFAULT_MEAN / 3
-DEFAULT_VARIANCE = DEFAULT_STD_DEV ** 2
-
-SKILL_CLASS_WIDTH = (DEFAULT_STD_DEV / 2) ** 2
-ADDITIVE_DYNAMICS_FACTOR = (DEFAULT_STD_DEV / 100) ** 2
-DRAW_PROBABILITY = 0.01  # 1 percent
-TOLERANCE = 1e-18
-
-STANDARD_NORMAL = NormalDist(0, 1)
+from constants import ADDITIVE_DYNAMICS_FACTOR
+from constants import DEFAULT_MEAN
+from constants import DEFAULT_STD_DEV
+from constants import SKILL_CLASS_WIDTH
+from trueskill_math import compute_draw_margin
+from trueskill_math import truncated_onesided_gaussian_v
+from trueskill_math import truncated_onesided_gaussian_w
+from trueskill_math import truncated_twosided_gaussian_v
+from trueskill_math import truncated_twosided_gaussian_w
 
 
 @dataclass(frozen=True)
 class Rating:
     mean: float = DEFAULT_MEAN
     stddev: float = DEFAULT_STD_DEV
-
-
-def compute_draw_margin() -> float:
-    '''
-      The margin to use to consider the game a draw, based on the (pre-set)
-      probability of a draw, which is typically set by measuring the draw rates
-      of a large number of games. Derived by inverting the formula
-
-        P(draw) = -1 + 2 * normal_cdf(
-            draw_margin / sqrt(player_count * skill_class_width)
-        )
-
-      or, as written exactly in the paper,
-
-        P(draw) = -1 + 2 * normal_cdf(
-            draw_margin / (sqrt(n1 + n2) * beta)
-        )
-    '''
-    inv_cdf_arg = 0.5 * (DRAW_PROBABILITY + 1)
-    inv_cdf_output = STANDARD_NORMAL.inv_cdf(inv_cdf_arg)
-    return inv_cdf_output * sqrt(2 * SKILL_CLASS_WIDTH)
-
-
-# TODO: add plots for these...
-def truncated_onesided_gaussian_v(t: float, lower: float) -> float:
-    '''Equation 4.7 from Herbrich '05, On Gaussian Expectation Propagation.
-
-    Representing the additive correction factor to the mean of a rectified
-    Gaussian that is only truncated from below.
-
-    This function grows roughly like lower - t for t < lower and quickly
-    approaches zero for t > lower.
-    '''
-    normalization = STANDARD_NORMAL.cdf(t - lower)
-    if normalization < TOLERANCE:
-        return lower - t   # approaches infinity
-    return STANDARD_NORMAL.pdf(t - lower) / normalization
-
-
-def truncated_onesided_gaussian_w(t: float, lower: float) -> float:
-    '''Equation 4.8 from Herbrich '05, On Gaussian Expectation Propagation.
-
-    Representing the additive correction factor to the variance of a rectified
-    Gaussian that is only truncated from below. This is a smooth approximation
-    to an indicator function for the condition t <= lower.
-    '''
-    v_value = truncated_onesided_gaussian_v(t, lower)
-    return v_value * (v_value + t - lower)
-
-
-def truncated_twosided_gaussian_v(t, lower, upper):
-    '''Equation 4.4 from Herbrich '05, On Gaussian Expectation Propagation.
-
-    Representing the additive correction factor to the mean of a rectified
-    Gaussian that is truncated on both sides.
-    '''
-    normalization = (STANDARD_NORMAL.cdf(upper - t) -
-                     STANDARD_NORMAL.cdf(lower - t))
-    if normalization < TOLERANCE:
-        # the limit as upper -> lower
-        return lower - t
-
-    return (
-        STANDARD_NORMAL.pdf(lower - t) - STANDARD_NORMAL.pdf(upper - t)
-    ) / normalization
-
-
-def truncated_twosided_gaussian_w(perf_diff, draw_margin):
-    '''Equation 4.4 from Herbrich '05, On Gaussian Expectation Propagation.
-
-    Representing the additive correction factor to the variance of a rectified
-    Gaussian that is truncated on both sides.
-    '''
-    abs_diff = abs(perf_diff)
-    normalization = (
-        STANDARD_NORMAL.cdf(draw_margin - abs_diff) -
-        STANDARD_NORMAL.cdf(-draw_margin - abs_diff)
-    )
-    if normalization < TOLERANCE:
-        return 1
-
-    v_value = truncated_twosided_gaussian_v(
-        perf_diff, -draw_margin, draw_margin)
-    t1 = (draw_margin - abs_diff) * STANDARD_NORMAL.pdf(draw_margin - abs_diff)
-    t2 = (-draw_margin - abs_diff) * \
-        STANDARD_NORMAL.pdf(-draw_margin - abs_diff)
-    return v_value ** 2 + (t1 - t2) / normalization
 
 
 def update_one_player(p1_rating, p2_rating, outcome):
